9f21ad4870898f22427162d6a3fa11db679e9826
Roman <kovalchuk.roman03@gmail.com>
2023-01-11 22:07:35 +0200

    Added run, stop icons.

--- Coursework/MainForm.h
+++ Coursework/MainForm.h
@@ -44,23 +44,25 @@ namespace Coursework {
 	private: System::Windows::Forms::MenuStrip^ menuStrip1;
 	protected:
 	private: System::Windows::Forms::ToolStripMenuItem^ ôàéëToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^ øîñüToolStripMenuItem;
+
 	private: System::Windows::Forms::ToolStripMenuItem^ ïðîÏðîãðàìóToolStripMenuItem;
 	private: System::Windows::Forms::ToolStrip^ toolStrip1;
 	private: System::Windows::Forms::StatusStrip^ statusStrip1;
 	private: System::Windows::Forms::ToolStripMenuItem^ çáåðåãòèToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ âèõ³äToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripButton^ toolStripStartLog;
+	private: System::Windows::Forms::ToolStripButton^ toolStripStopLog;
 
 
 
 
-	private: System::Windows::Forms::ToolStripButton^ toolStripButton1;
+
 	private: System::Windows::Forms::ToolStripButton^ toolStripButton2;
 	private: System::Windows::Forms::ToolStripButton^ toolStripButton3;
-	private: System::Windows::Forms::ToolStripButton^ toolStripButton4;
-	private: System::Windows::Forms::ToolStripProgressBar^ toolStripProgressBar1;
-	private: System::Windows::Forms::ToolStripStatusLabel^ toolStripStatusLabel1;
+
+	private: System::Windows::Forms::ToolStripStatusLabel^ loggingMode;
+
+
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
 	private: System::Windows::Forms::RichTextBox^ richTextBox1;
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel2;
@@ -86,17 +88,14 @@ namespace Coursework {
 			this->ôàéëToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->çáåðåãòèToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->âèõ³äToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->øîñüToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->ïðîÏðîãðàìóToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->toolStrip1 = (gcnew System::Windows::Forms::ToolStrip());
 			this->toolStripStartLog = (gcnew System::Windows::Forms::ToolStripButton());
-			this->toolStripButton1 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->toolStripStopLog = (gcnew System::Windows::Forms::ToolStripButton());
 			this->toolStripButton2 = (gcnew System::Windows::Forms::ToolStripButton());
 			this->toolStripButton3 = (gcnew System::Windows::Forms::ToolStripButton());
-			this->toolStripButton4 = (gcnew System::Windows::Forms::ToolStripButton());
 			this->statusStrip1 = (gcnew System::Windows::Forms::StatusStrip());
-			this->toolStripProgressBar1 = (gcnew System::Windows::Forms::ToolStripProgressBar());
-			this->toolStripStatusLabel1 = (gcnew System::Windows::Forms::ToolStripStatusLabel());
+			this->loggingMode = (gcnew System::Windows::Forms::ToolStripStatusLabel());
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->tableLayoutPanel2 = (gcnew System::Windows::Forms::TableLayoutPanel());
@@ -112,14 +111,14 @@ namespace Coursework {
 			// 
 			this->menuStrip1->GripMargin = System::Windows::Forms::Padding(2, 2, 0, 2);
 			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
 				this->ôàéëToolStripMenuItem,
-					this->øîñüToolStripMenuItem, this->ïðîÏðîãðàìóToolStripMenuItem
+					this->ïðîÏðîãðàìóToolStripMenuItem
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
 			this->menuStrip1->Padding = System::Windows::Forms::Padding(7, 2, 0, 2);
-			this->menuStrip1->Size = System::Drawing::Size(1051, 36);
+			this->menuStrip1->Size = System::Drawing::Size(1051, 33);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -136,20 +135,14 @@ namespace Coursework {
 			// çáåðåãòèToolStripMenuItem
 			// 
 			this->çáåðåãòèToolStripMenuItem->Name = L"çáåðåãòèToolStripMenuItem";
-			this->çáåðåãòèToolStripMenuItem->Size = System::Drawing::Size(187, 34);
+			this->çáåðåãòèToolStripMenuItem->Size = System::Drawing::Size(270, 34);
 			this->çáåðåãòèToolStripMenuItem->Text = L"Çáåðåãòè";
 			// 
 			// âèõ³äToolStripMenuItem
 			// 
 			this->âèõ³äToolStripMenuItem->Name = L"âèõ³äToolStripMenuItem";
-			this->âèõ³äToolStripMenuItem->Size = System::Drawing::Size(187, 34);
+			this->âèõ³äToolStripMenuItem->Size = System::Drawing::Size(270, 34);
 			this->âèõ³äToolStripMenuItem->Text = L"Âèõ³ä";
-			// 
-			// øîñüToolStripMenuItem
-			// 
-			this->øîñüToolStripMenuItem->Name = L"øîñüToolStripMenuItem";
-			this->øîñüToolStripMenuItem->Size = System::Drawing::Size(73, 29);
-			this->øîñüToolStripMenuItem->Text = L"Øîñü";
 			// 
 			// ïðîÏðîãðàìóToolStripMenuItem
 			// 
@@ -160,13 +153,13 @@ namespace Coursework {
 			// toolStrip1
 			// 
 			this->toolStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
-			this->toolStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(5) {
+			this->toolStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
 				this->toolStripStartLog,
-					this->toolStripButton1, this->toolStripButton2, this->toolStripButton3, this->toolStripButton4
+					this->toolStripStopLog, this->toolStripButton2, this->toolStripButton3
 			});
-			this->toolStrip1->Location = System::Drawing::Point(0, 36);
+			this->toolStrip1->Location = System::Drawing::Point(0, 33);
 			this->toolStrip1->Name = L"toolStrip1";
-			this->toolStrip1->Size = System::Drawing::Size(1051, 38);
+			this->toolStrip1->Size = System::Drawing::Size(1051, 29);
 			this->toolStrip1->TabIndex = 1;
 			this->toolStrip1->Text = L"toolStrip1";
 			// 
@@ -176,18 +169,18 @@ namespace Coursework {
 			this->toolStripStartLog->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripStartLog.Image")));
 			this->toolStripStartLog->ImageTransparentColor = System::Drawing::Color::Magenta;
 			this->toolStripStartLog->Name = L"toolStripStartLog";
-			this->toolStripStartLog->Size = System::Drawing::Size(34, 33);
+			this->toolStripStartLog->Size = System::Drawing::Size(34, 24);
 			this->toolStripStartLog->Text = L"toolStripButton1";
 			this->toolStripStartLog->Click += gcnew System::EventHandler(this, &MainForm::toolStripButton1_Click);
 			// 
-			// toolStripButton1
+			// toolStripStopLog
 			// 
-			this->toolStripButton1->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
-			this->toolStripButton1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton1.Image")));
-			this->toolStripButton1->ImageTransparentColor = System::Drawing::Color::Magenta;
-			this->toolStripButton1->Name = L"toolStripButton1";
-			this->toolStripButton1->Size = System::Drawing::Size(34, 24);
-			this->toolStripButton1->Text = L"toolStripEndLog";
+			this->toolStripStopLog->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->toolStripStopLog->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripStopLog.Image")));
+			this->toolStripStopLog->ImageTransparentColor = System::Drawing::Color::Magenta;
+			this->toolStripStopLog->Name = L"toolStripStopLog";
+			this->toolStripStopLog->Size = System::Drawing::Size(34, 24);
+			this->toolStripStopLog->Text = L"toolStripEndLog";
 			// 
 			// toolStripButton2
 			// 
@@ -207,22 +200,10 @@ namespace Coursework {
 			this->toolStripButton3->Size = System::Drawing::Size(34, 24);
 			this->toolStripButton3->Text = L"toolStripRefresh";
 			// 
-			// toolStripButton4
-			// 
-			this->toolStripButton4->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
-			this->toolStripButton4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton4.Image")));
-			this->toolStripButton4->ImageTransparentColor = System::Drawing::Color::Magenta;
-			this->toolStripButton4->Name = L"toolStripButton4";
-			this->toolStripButton4->Size = System::Drawing::Size(34, 24);
-			this->toolStripButton4->Text = L"toolStripClearLog";
-			// 
 			// statusStrip1
 			// 
 			this->statusStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
-			this->statusStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
-				this->toolStripProgressBar1,
-					this->toolStripStatusLabel1
-			});
+			this->statusStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->loggingMode });
 			this->statusStrip1->Location = System::Drawing::Point(0, 550);
 			this->statusStrip1->Name = L"statusStrip1";
 			this->statusStrip1->Padding = System::Windows::Forms::Padding(1, 0, 16, 0);
@@ -230,16 +211,11 @@ namespace Coursework {
 			this->statusStrip1->TabIndex = 2;
 			this->statusStrip1->Text = L"statusStrip1";
 			// 
-			// toolStripProgressBar1
+			// loggingMode
 			// 
-			this->toolStripProgressBar1->Name = L"toolStripProgressBar1";
-			this->toolStripProgressBar1->Size = System::Drawing::Size(112, 24);
-			// 
-			// toolStripStatusLabel1
-			// 
-			this->toolStripStatusLabel1->Name = L"toolStripStatusLabel1";
-			this->toolStripStatusLabel1->Size = System::Drawing::Size(179, 25);
-			this->toolStripStatusLabel1->Text = L"toolStripStatusLabel1";
+			this->loggingMode->Name = L"loggingMode";
+			this->loggingMode->Size = System::Drawing::Size(189, 25);
+			this->loggingMode->Text = L"Ëîãóâàííÿ óâ³ìêíåíî";
 			// 
 			// tableLayoutPanel1
 			// 
@@ -251,11 +227,11 @@ namespace Coursework {
 			this->tableLayoutPanel1->Controls->Add(this->richTextBox1, 1, 0);
 			this->tableLayoutPanel1->Controls->Add(this->tableLayoutPanel2, 0, 0);
 			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 74);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 62);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 1;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(1051, 476);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(1051, 488);
 			this->tableLayoutPanel1->TabIndex = 3;
 			// 
 			// richTextBox1
@@ -265,7 +241,7 @@ namespace Coursework {
 			this->richTextBox1->Margin = System::Windows::Forms::Padding(3, 3, 20, 10);
 			this->richTextBox1->Name = L"richTextBox1";
 			this->richTextBox1->ReadOnly = true;
-			this->richTextBox1->Size = System::Drawing::Size(779, 463);
+			this->richTextBox1->Size = System::Drawing::Size(779, 475);
 			this->richTextBox1->TabIndex = 0;
 			this->richTextBox1->Text = L"";
 			// 
@@ -281,13 +257,13 @@ namespace Coursework {
 			this->tableLayoutPanel2->RowCount = 2;
 			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 45.22821F)));
 			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 54.77179F)));
-			this->tableLayoutPanel2->Size = System::Drawing::Size(243, 470);
+			this->tableLayoutPanel2->Size = System::Drawing::Size(243, 482);
 			this->tableLayoutPanel2->TabIndex = 1;
 			// 
 			// monthCalendar1
 			// 
 			this->monthCalendar1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->monthCalendar1->Location = System::Drawing::Point(9, 221);
+			this->monthCalendar1->Location = System::Drawing::Point(9, 227);
 			this->monthCalendar1->Name = L"monthCalendar1";
 			this->monthCalendar1->TabIndex = 0;
 			// 
